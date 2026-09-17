2048b4175899302dbd1d748617a81609736dc8d2
Andreas Wallgren <anwa2301@student.miun.se>
2026-09-17 23:57:54 +0200

    test(SeekerServiceTest): remove unused NotificationService mock

--- src/test/java/com/walkmates/lab2/SeekerServiceTest.java
+++ src/test/java/com/walkmates/lab2/SeekerServiceTest.java
@@ -30,9 +30,6 @@ class SeekerServiceTest {
     @Mock
     private PaymentService paymentService;
 
-    @Mock
-    private NotificationService notificationService;
-
     @InjectMocks
     private SeekerService seekerService;
 
